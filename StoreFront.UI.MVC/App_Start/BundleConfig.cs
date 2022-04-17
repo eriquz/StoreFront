089bcdbae4597573c4f936d5122a369354bb8e9e
Enrique Garcia <EriquzGar10@outlook.com>
2022-04-17 16:50:04 -0500

    Added.Identity Samples & Started Cleaning up.

--- StoreFront.UI.MVC/App_Start/BundleConfig.cs
+++ StoreFront.UI.MVC/App_Start/BundleConfig.cs
@@ -8,23 +8,31 @@ namespace StoreFront.UI.MVC
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Content/vendor/jquery/jquery.min.js",
+                        "~/Content/assets/js/custom.js",
+                      "~/Content/assets/js/owl.js",
+                      "~/Content/assets/js/slick.js",
+                      "~/Content/assets/js/isotope.js",
+                      "~/Content/assets/js/accordions.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Content/vendor/bootstrap/js/bootstrap.bundle.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/assets/css/fontawesome.css",
+                      "~/Content/assets/css/templatemo-sixteen.css",
+                      "~/Content/assets/css/owl.css"
+                      ));
         }
     }
 }
